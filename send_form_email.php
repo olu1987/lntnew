d952eb4d44f832467e87132a479499cc5081fae6
Olu Meduoye <olu@claymoreminds.com>
2017-04-02 11:22:59 +0100

    lnt email testing

--- send_form_email.php
+++ send_form_email.php
@@ -19,6 +19,72 @@
 <body class="home">
 <div class="slideshow-wrapper">
     <div id="slideshow" class="container-fluid">
+    </div>
+    <!--[if lt IE 8]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+
+    <!-- Add your site or application content here -->
+    <div class="navigation text-center">
+        <ul class="list-inline text-right toggle-button">
+            <li><a data-toggle="collapse" data-target="#nav" class="btn fa fa-bars"></a></li>
+        </ul>
+        <ul id="nav" class="list-inline collapse text-left">
+            <li><a href="index.html">. LNT . LONDON .</a></li>
+            <li><a href="about.html">. ABOUT .</a></li>
+            <li><a href="prints.html">. PRINTS .</a></li>
+            <li><a href="accessories.html">. ACCESSORIES .</a></li>
+            <li><a href="clothing.html">. CLOTHING .</a></li>
+            <li><a href="#">. BLOG .</a></li>
+            <li><a href="#">. PRESS .</a></li>
+            <li><a href="#">. CONTACT .</a></li>
+            <li class="social">
+                <a target="_blank" href="https://en-gb.facebook.com/lntlondon/"><i class="fa fa-facebook"></i></a>
+                <a target="_blank" href="https://twitter.com/LNTLondon"><i class="fa fa-twitter"></i></a>
+                <a target="_blank" href="https://www.instagram.com/lntlondon/?hl=en"><i class="fa fa-instagram"></i></a>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <footer class="row">
+        <div class="col-md-12">
+            <ul>
+                <li><a href="index.html">. LNT . LONDON .</a></li>
+                <li><a href="about.html">. ABOUT .</a></li>
+                <li><a href="#">. SITEMAP .</a></li>
+                <li><a href="#">. BLOG .</a></li>
+                <li><a href="#">. PRESS .</a></li>
+                <li><a href="#">. CONTACT .</a></li>
+                <li class="social">
+                    <a target="_blank" href="https://en-gb.facebook.com/lntlondon/"><i class="fa fa-facebook"></i></a>
+                    <a target="_blank" href="https://twitter.com/LNTLondon"><i class="fa fa-twitter"></i></a>
+                    <a target="_blank" href="https://www.instagram.com/lntlondon/?hl=en"><i class="fa fa-instagram"></i></a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+</script>
+</body>
+</html>
 
 <?php
 if(isset($_POST['email'])) {
@@ -107,6 +173,7 @@ if(isset($_POST['email'])) {
     $email_message .= "Telephone: ".clean_string($telephone)."\n";
     $email_message .= "Comments: ".clean_string($comments)."\n";
     $email_message .= "Country: ".clean_string($country)."\n";
+    $email_message .= "Subject: ".clean_string($subject)."\n";
 
 // create email headers
     $headers = 'From: '.$email_from."\r\n".
@@ -121,69 +188,4 @@ if(isset($_POST['email'])) {
 
 }
 ?>
-    </div>
-    <!--[if lt IE 8]>
-    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-    <![endif]-->
 
-    <!-- Add your site or application content here -->
-    <div class="navigation text-center">
-        <ul class="list-inline text-right toggle-button">
-            <li><a data-toggle="collapse" data-target="#nav" class="btn fa fa-bars"></a></li>
-        </ul>
-        <ul id="nav" class="list-inline collapse text-left">
-            <li><a href="index.html">. LNT . LONDON .</a></li>
-            <li><a href="about.html">. ABOUT .</a></li>
-            <li><a href="prints.html">. PRINTS .</a></li>
-            <li><a href="accessories.html">. ACCESSORIES .</a></li>
-            <li><a href="clothing.html">. CLOTHING .</a></li>
-            <li><a href="#">. BLOG .</a></li>
-            <li><a href="#">. PRESS .</a></li>
-            <li><a href="#">. CONTACT .</a></li>
-            <li class="social">
-                <a target="_blank" href="https://en-gb.facebook.com/lntlondon/"><i class="fa fa-facebook"></i></a>
-                <a target="_blank" href="https://twitter.com/LNTLondon"><i class="fa fa-twitter"></i></a>
-                <a target="_blank" href="https://www.instagram.com/lntlondon/?hl=en"><i class="fa fa-instagram"></i></a>
-            </li>
-        </ul>
-    </div>
-</div>
-
-<div class="container-fluid">
-    <footer class="row">
-        <div class="col-md-12">
-            <ul>
-                <li><a href="index.html">. LNT . LONDON .</a></li>
-                <li><a href="about.html">. ABOUT .</a></li>
-                <li><a href="#">. SITEMAP .</a></li>
-                <li><a href="#">. BLOG .</a></li>
-                <li><a href="#">. PRESS .</a></li>
-                <li><a href="#">. CONTACT .</a></li>
-                <li class="social">
-                    <a target="_blank" href="https://en-gb.facebook.com/lntlondon/"><i class="fa fa-facebook"></i></a>
-                    <a target="_blank" href="https://twitter.com/LNTLondon"><i class="fa fa-twitter"></i></a>
-                    <a target="_blank" href="https://www.instagram.com/lntlondon/?hl=en"><i class="fa fa-instagram"></i></a>
-                </li>
-            </ul>
-        </div>
-    </footer>
-</div>
-
-
-<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="js/plugins.js"></script>
-<script src="js/main.js"></script>
-
-<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-<script>
-    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-        e.src='https://www.google-analytics.com/analytics.js';
-        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-</script>
-</body>
-</html>
