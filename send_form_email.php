d67c5b0f61b81c347aeb2b007ca5daa165a0fa8e
Olu Meduoye <olu@claymoreminds.com>
2017-04-02 11:08:54 +0100

    lnt email test

--- send_form_email.php
+++ send_form_email.php
@@ -93,7 +93,101 @@ if(isset($_POST['email'])) {
     @mail($email_to, $email_subject, $email_message, $headers);
     ?>
 
-    <!-- include your own success html here -->
+    <!doctype html>
+    <html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>HOME | LNT LONDON</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+        <link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body class="home">
+    <div class="slideshow-wrapper">
+        <div id="slideshow" class="container-fluid">
+
+            <div class="row home-bg pic-one">
+            </div>
+            <div class="row home-bg pic-two">
+            </div>
+            <div class="row home-bg pic-three">
+            </div>
+        </div>
+        <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <div class="navigation text-center">
+            <ul class="list-inline text-right toggle-button">
+                <li><a data-toggle="collapse" data-target="#nav" class="btn fa fa-bars"></a></li>
+            </ul>
+            <ul id="nav" class="list-inline collapse text-left">
+                <li><a href="index.html">. LNT . LONDON .</a></li>
+                <li><a href="about.html">. ABOUT .</a></li>
+                <li><a href="prints.html">. PRINTS .</a></li>
+                <li><a href="accessories.html">. ACCESSORIES .</a></li>
+                <li><a href="clothing.html">. CLOTHING .</a></li>
+                <li><a href="#">. BLOG .</a></li>
+                <li><a href="#">. PRESS .</a></li>
+                <li><a href="#">. CONTACT .</a></li>
+                <li class="social">
+                    <a target="_blank" href="https://en-gb.facebook.com/lntlondon/"><i class="fa fa-facebook"></i></a>
+                    <a target="_blank" href="https://twitter.com/LNTLondon"><i class="fa fa-twitter"></i></a>
+                    <a target="_blank" href="https://www.instagram.com/lntlondon/?hl=en"><i class="fa fa-instagram"></i></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <footer class="row">
+            <div class="col-md-12">
+                <ul>
+                    <li><a href="index.html">. LNT . LONDON .</a></li>
+                    <li><a href="about.html">. ABOUT .</a></li>
+                    <li><a href="#">. SITEMAP .</a></li>
+                    <li><a href="#">. BLOG .</a></li>
+                    <li><a href="#">. PRESS .</a></li>
+                    <li><a href="#">. CONTACT .</a></li>
+                    <li class="social">
+                        <a target="_blank" href="https://en-gb.facebook.com/lntlondon/"><i class="fa fa-facebook"></i></a>
+                        <a target="_blank" href="https://twitter.com/LNTLondon"><i class="fa fa-twitter"></i></a>
+                        <a target="_blank" href="https://www.instagram.com/lntlondon/?hl=en"><i class="fa fa-instagram"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+    </script>
+    </body>
+    </html>
+
 
     Thank you for contacting us. We will be in touch with you very soon.
 
