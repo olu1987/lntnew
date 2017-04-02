cb99e7d6ddde39d878d2a031f27505cea4fc056a
Olu Meduoye <olu@claymoreminds.com>
2017-04-02 11:17:19 +0100

    more styling for contact

--- send_form_email.php
+++ send_form_email.php
@@ -114,22 +114,48 @@ if(isset($_POST['email'])) {
         'X-Mailer: PHP/' . phpversion();
     @mail($email_to, $email_subject, $email_message, $headers);
     ?>
-        </div>
-        <!--[if lt IE 8]>
-        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
 
-        <!-- Add your site or application content here -->
-        <div class="navigation text-center">
-            <ul class="list-inline text-right toggle-button">
-                <li><a data-toggle="collapse" data-target="#nav" class="btn fa fa-bars"></a></li>
-            </ul>
-            <ul id="nav" class="list-inline collapse text-left">
+    Thank you for contacting us. We will be in touch with you very soon.
+
+    <?php
+
+}
+?>
+    </div>
+    <!--[if lt IE 8]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+
+    <!-- Add your site or application content here -->
+    <div class="navigation text-center">
+        <ul class="list-inline text-right toggle-button">
+            <li><a data-toggle="collapse" data-target="#nav" class="btn fa fa-bars"></a></li>
+        </ul>
+        <ul id="nav" class="list-inline collapse text-left">
+            <li><a href="index.html">. LNT . LONDON .</a></li>
+            <li><a href="about.html">. ABOUT .</a></li>
+            <li><a href="prints.html">. PRINTS .</a></li>
+            <li><a href="accessories.html">. ACCESSORIES .</a></li>
+            <li><a href="clothing.html">. CLOTHING .</a></li>
+            <li><a href="#">. BLOG .</a></li>
+            <li><a href="#">. PRESS .</a></li>
+            <li><a href="#">. CONTACT .</a></li>
+            <li class="social">
+                <a target="_blank" href="https://en-gb.facebook.com/lntlondon/"><i class="fa fa-facebook"></i></a>
+                <a target="_blank" href="https://twitter.com/LNTLondon"><i class="fa fa-twitter"></i></a>
+                <a target="_blank" href="https://www.instagram.com/lntlondon/?hl=en"><i class="fa fa-instagram"></i></a>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <footer class="row">
+        <div class="col-md-12">
+            <ul>
                 <li><a href="index.html">. LNT . LONDON .</a></li>
                 <li><a href="about.html">. ABOUT .</a></li>
-                <li><a href="prints.html">. PRINTS .</a></li>
-                <li><a href="accessories.html">. ACCESSORIES .</a></li>
-                <li><a href="clothing.html">. CLOTHING .</a></li>
+                <li><a href="#">. SITEMAP .</a></li>
                 <li><a href="#">. BLOG .</a></li>
                 <li><a href="#">. PRESS .</a></li>
                 <li><a href="#">. CONTACT .</a></li>
@@ -140,51 +166,24 @@ if(isset($_POST['email'])) {
                 </li>
             </ul>
         </div>
-    </div>
-
-    <div class="container-fluid">
-        <footer class="row">
-            <div class="col-md-12">
-                <ul>
-                    <li><a href="index.html">. LNT . LONDON .</a></li>
-                    <li><a href="about.html">. ABOUT .</a></li>
-                    <li><a href="#">. SITEMAP .</a></li>
-                    <li><a href="#">. BLOG .</a></li>
-                    <li><a href="#">. PRESS .</a></li>
-                    <li><a href="#">. CONTACT .</a></li>
-                    <li class="social">
-                        <a target="_blank" href="https://en-gb.facebook.com/lntlondon/"><i class="fa fa-facebook"></i></a>
-                        <a target="_blank" href="https://twitter.com/LNTLondon"><i class="fa fa-twitter"></i></a>
-                        <a target="_blank" href="https://www.instagram.com/lntlondon/?hl=en"><i class="fa fa-instagram"></i></a>
-                    </li>
-                </ul>
-            </div>
-        </footer>
-    </div>
+    </footer>
+</div>
 
 
-    <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="js/plugins.js"></script>
-    <script src="js/main.js"></script>
+<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
 
-    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-    <script>
-        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='https://www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-        ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-    </script>
-    </body>
-    </html>
-
-
-    Thank you for contacting us. We will be in touch with you very soon.
-
-    <?php
-
-}
-?>
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+</script>
+</body>
+</html>
