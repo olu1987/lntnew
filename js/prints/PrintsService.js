20a8fabb3af8f7fbe4fd06778492cb5d99a60620
Olu Meduoye <olu@claymoreminds.com>
2017-04-23 10:13:08 +0100

    prints fixes

--- js/prints/PrintsService.js
+++ js/prints/PrintsService.js
@@ -2,7 +2,7 @@ var AccesoriesService = angular.module('PrintsService', [])
     .service('Prints', function () {
         this.items = [
             {   id:1,
-                name:'Bert Square',
+                name:'Bert Original',
                 character:'Bert',
                 url:'img/ap-bert-opt.jpg',
                 price:'150.00',
@@ -12,14 +12,14 @@ var AccesoriesService = angular.module('PrintsService', [])
 
             {
                 id:4,
-                name:'Bert Orange and Petrol Square',
+                name:'Timothy Original',
                 character:'Timothy',
                 url:'img/pink-tiger-opt.jpg',
                 price:'50.00',
                 column:'col-md-8 col-sm-6 col-xs-12'
             },{
                 id:2,
-                name:'Bert Soft Square',
+                name:'Bert Orange Green',
                 character:'Bert',
                 url:'img/bert-2-opt.jpg',
                 price:'150.00',
@@ -27,14 +27,14 @@ var AccesoriesService = angular.module('PrintsService', [])
             },
             {
                 id:5,
-                name:'Bert Purple and Emerald Square',
+                name:'Timothy light Orange',
                 character:'Timothy',
                 url:'img/tiger-one-opt.jpg',
                 price:'50.00',
                 column:'col-md-8 col-sm-6 col-xs-12'
             },   {
                 id:3,
-                name:'Bert Blue Green Square',
+                name:'Bert Enhanced',
                 character:'Bert',
                 url:'img/original-bert-opt.jpg',
                 price:'150.00',
@@ -42,24 +42,24 @@ var AccesoriesService = angular.module('PrintsService', [])
             },
             {
                 id:6,
-                name:'Bert Soft Pink Square',
-                character:'Bert',
+                name:'Violet Rain Original',
+                character:'Violet',
                 url:'img/vr-3-opt.jpg',
                 price:'50.00',
                 column:'col-md-8 col-sm-6 col-xs-12'
             },
             {
                 id:7,
-                name:'Timothy Cosmic Aqua Square',
-                character:'Timothy',
+                name:'Violet Rain Cosmic Aqua',
+                character:'Violet',
                 url:'img/vr-aqua-opt.jpg',
                 price:'50.00',
                 column:'col-md-8 col-sm-6 col-xs-12'
             },
             {
                 id:8,
-                name:'Timothy Cosmic Aqua Square',
-                character:'Timothy',
+                name:'Violet Rain Light',
+                character:'Violet',
                 url:'img/VR-purple-opt.jpg',
                 price:'50.00',
                 column:'col-md-8 col-sm-6 col-xs-12'
