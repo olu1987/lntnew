9ce45c2a7cffae452a3fdea2056e0eead3838e0b
Olu Meduoye <olu@claymoreminds.com>
2017-08-08 17:58:40 +0100

    finishing clothing page

--- js/accessories/AccessoriesService.js
+++ js/accessories/AccessoriesService.js
@@ -1,57 +1,9 @@
 var AccesoriesService = angular.module('AccesoriesService', [])
     .service('Accessories', function () {
         this.items = [
-            {   id:1,
-                name:'Bert Square',
-                character:'Bert',
-                url:'img/bert-pocket-square.jpg',
-                price:'50.00'
-            },
-            {
-                id:2,
-                name:'Bert Soft Square',
-                character:'Bert',
-                url:'img/bert-soft-square.jpg',
-                price:'50.00'
-            },
-            {
-                id:3,
-                name:'Bert Blue Green Square',
-                character:'Bert',
-                url:'img/bert-blue-green-square.jpg',
-                price:'50.00'
-            },
-            {
-                id:4,
-                name:'Bert Orange and Petrol Square',
-                character:'Bert',
-                url:'img/bert-orange-and-petrol.jpg',
-                price:'50.00'
-            },
-            {
-                id:5,
-                name:'Bert Purple and Emerald Square',
-                character:'Bert',
-                url:'img/bert-purple-and-emerald.jpg',
-                price:'50.00'
-            },
-            {
-                id:6,
-                name:'Bert Soft Pink Square',
-                character:'Bert',
-                url:'img/bert-soft-pink-square.jpg',
-                price:'50.00'
-            },
-            {
-                id:7,
-                name:'Timothy Cosmic Aqua Square',
-                character:'Timothy',
-                url:'img/timothy-aqua-square.jpg',
-                price:'50.00'
-            },
             {
                 id:8,
-                name:'Timothy Cosmic Green Square',
+                name:'',
                 character:'Timothy',
                 url:'img/timothy-green-square.jpg',
                 price:'50.00'
