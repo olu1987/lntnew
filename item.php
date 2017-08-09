2cf964db940fb57bddb21d561158ba3c7f71d267
Olu Meduoye <olu@claymoreminds.com>
2017-08-09 08:21:00 +0100

    moving connection to configs

--- item.php
+++ item.php
@@ -17,36 +17,8 @@
     <script src="js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
 <body class="item">
+<?php include 'configs/connect.php'; ?>
 <?php
-function db_connect() {
-
-    // Define connection as a static variable, to avoid connecting more than once
-    static $connection;
-
-    // Try and connect to the database, if a connection has not been established yet
-    if(!isset($connection)) {
-        // Load configuration as an array. Use the actual location of your configuration file
-        $config = parse_ini_file('/configs/config.ini');
-        $connection = mysqli_connect('localhost',$config['username'],$config['password'],$config['dbname']);
-    }
-
-    // If connection was not successful, handle the error
-    if($connection === false) {
-        // Handle error - notify administrator, log to a file, show an error screen, etc.
-        return mysqli_connect_error();
-    }
-    return $connection;
-}
-
-function db_query($query) {
-    // Connect to the database
-    $connection = db_connect();
-
-    // Query the database
-    $result = mysqli_query($connection,$query);
-
-    return $result;
-}
 
 // A select query. $result will be a `mysqli_result` object if successful
 $result = db_query("SELECT * FROM ".$_GET['table']." WHERE id = ".$_GET['id']);
