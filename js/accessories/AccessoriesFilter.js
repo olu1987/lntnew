3c5dcb0e6c89fb924d118b7b4c388f3feea8cbac
Olu Meduoye <olu.meduoye@tvplayer.com>
2017-12-10 13:56:28 +0000

    correcting filtering on accessories page

--- js/accessories/AccessoriesFilter.js
+++ js/accessories/AccessoriesFilter.js
@@ -7,6 +7,17 @@ app.filter('chooseCharacter', function () {
             }
         });
         return filtered;
+    };
+});
 
+app.filter('chooseItem', function () {
+    return function (input, str) {
+        var filtered = [];
+        angular.forEach(input, function(item) {
+            if(item.item_type.toLowerCase() == str || str == undefined) {
+                filtered.push(item);
+            }
+        });
+        return filtered;
     };
 });
