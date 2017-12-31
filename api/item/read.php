78c88788ef795f09287f4436f39b2100b4f76aa0
Olu Meduoye <olu.meduoye@tvplayer.com>
2017-12-31 14:22:47 +0000

    api methods and classes

---
+++ api/item/read.php
@@ -0,0 +1,58 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/item.php';
+
+// instantiate database and product object
+$database = new Database();
+$db = $database->getConnection();
+
+// initialize object
+$product = new PrintSingle($db);
+
+// query products
+$stmt = $product->read();
+$num = $stmt->rowCount();
+
+// check if more than 0 record found
+if($num>0){
+
+    // products array
+    $products_arr=array();
+    $products_arr["records"]=array();
+
+    // retrieve our table contents
+    // fetch() is faster than fetchAll()
+    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        // extract row
+        // this will make $row['name'] to
+        // just $name only
+        extract($row);
+
+        $product_item=array(
+            "id" => $id,
+            "item_name" => $item_name,
+            "item_description" => html_entity_decode($item_description),
+            "item_price" => $item_price,
+            "character_name;" => $character_name,
+            "item_image_url;" => $item_image_url,
+            "button_id" => $button_id
+        );
+
+        array_push($products_arr["records"], $product_item);
+    }
+
+    echo json_encode($products_arr);
+}
+
+else{
+    echo json_encode(
+        array("message" => "No items found.")
+    );
+}
+?>
---
+++ api/objects/item.php
@@ -0,0 +1,39 @@
+<?php
+class PrintSingle{
+
+
+    // database connection and table name
+    private $conn;
+
+    // object properties
+    public $id;
+    public $item_name;
+    public $item_description;
+    public $character_name;
+    public $item_image_url;
+    public $button_id;
+    public $item_price;
+
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+
+    // read products
+    function read(){
+
+        // select all query
+        $query = "SELECT
+                item_name, id, item_description, item_price, character_name, item_image_url, button_id
+            FROM
+                " . htmlspecialchars($_GET["table"]) . " ";
+
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+}
